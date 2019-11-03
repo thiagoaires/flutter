89b9041c42dbac606426996a43e963a5244d0e3a
thiago.aires <makarrao@gmail.com>
2019-11-03 00:27:11 -0300

    numeros inteiro

---
+++ Dart/sintaxe/intVariables.dart
@@ -0,0 +1,8 @@
+void main(){
+  int age = 35;
+  int brotherAge = age - 10;
+  int grandmaAge = (brotherAge + age) * 3;
+  print("$age eh minha idade");
+  print("a idade do meu irmao eh $brotherAge");
+  print("A idade da vovo eh $grandmaAge");
+}
