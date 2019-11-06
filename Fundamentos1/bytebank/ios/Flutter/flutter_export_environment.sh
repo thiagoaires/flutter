6c96e625b482acb12559fd9ea7fd06b739aa15a2
thiagoaires <makarrao@gmail.com>
2019-11-06 14:18:16 -0300

    flutter - bytebank[transferencia]

---
+++ Fundamentos1/bytebank/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thiago.aires/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thiago.aires/Documents/flutter/Fundamentos1/bytebank"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/thiago.aires/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- bytebank/flutter_app/lib/main.dart
+++ bytebank/flutter_app/lib/main.dart
@@ -1,29 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-          home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("Titulo"),
-              subtitle: Text("descricao"),
-            )),
-            Card(
-                child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text("Titulo"),
-              subtitle: Text("descricao"),
-            )),
-          ],
-        ),
-        appBar: AppBar(
-          title: Text("Transferencias"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.access_alarm),
-        ),
-      )),
+      CurrentApp(),
     );
+
+class CurrentApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: ListaTransferencias(),
+      appBar: AppBar(
+        title: Text("Transferencias"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.access_alarm),
+      ),
+    ));
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      ItemTransferencia(Transferencia(200.0, 312)),
+      ItemTransferencia(Transferencia(3000, 123)),
+      ItemTransferencia(Transferencia(10, 8499)),
+    ]);
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.agencia.toString()),
+    ));
+  }
+}
+
+class Transferencia{
+   final double valor;
+   final int agencia;
+
+   Transferencia(this.valor, this.agencia);
+}
