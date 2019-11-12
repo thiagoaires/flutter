563795796c686f3047fb85b1c2307c6c38c72e93
thiago.aires <makarrao@gmail.com>
2019-11-11 23:52:24 -0300

    Flutter - Primeiros passos

--- bytebank/flutter_app/lib/main.dart
+++ bytebank/flutter_app/lib/main.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      CurrentApp(),
+      ByteBank(),
     );
 
-class CurrentApp extends StatelessWidget {
+class ByteBank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
 
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Bytebank'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  hintText: '0000',
+                  labelText: 'Agencia'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  hintText: 'Valor',
+                  labelText: '0.00'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Confirmar'),
+            )
+          ],
+        ));
+  }
+}
+
+class CurrentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,14 +76,15 @@ class CurrentApp extends StatelessWidget {
 }
 
 class ListaTransferencias extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransferencia(Transferencia(200.0, 312)),
-      ItemTransferencia(Transferencia(3000, 123)),
-      ItemTransferencia(Transferencia(10, 8499)),
-    ]);
+    return Scaffold(
+      body: Column(children: <Widget>[
+        ItemTransferencia(Transferencia(200.0, 312)),
+        ItemTransferencia(Transferencia(3000, 123)),
+        ItemTransferencia(Transferencia(10, 8499)),
+      ]),
+    );
   }
 }
 
@@ -49,9 +104,9 @@ class ItemTransferencia extends StatelessWidget {
   }
 }
 
-class Transferencia{
-   final double valor;
-   final int agencia;
+class Transferencia {
+  final double valor;
+  final int agencia;
 
-   Transferencia(this.valor, this.agencia);
+  Transferencia(this.valor, this.agencia);
 }
