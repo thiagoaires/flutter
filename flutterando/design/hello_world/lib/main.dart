269da72b116e5fe49a81c97561df37b069af913f
thiagoaires <makarrao@gmail.com>
2019-11-22 18:43:14 -0300

    flutterando: row & column

--- flutterando/design/hello_world/lib/main.dart
+++ flutterando/design/hello_world/lib/main.dart
@@ -12,22 +12,52 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.yellow,
       ),
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 200,
-              color: Colors.yellow,
-              height: 39,
-            ),
-            Container(
-              color: Colors.red,
-              width: double.maxFinite,
-              height: 60,),
-            ButtonCard(),
-            TextButton()  
-          ]
+                  
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: <Widget>[
+              ButtonCard(),
+              Container(height: 50,),
+              TextButton(),
+              ButtonRowIcon(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(color: Colors.black45, width: 90, height: 99,),
+                  Container(color: Colors.deepOrangeAccent, width: 40, height: 22,),
+                   
+                ],
+              )
+            ]
+          ), 
+        ),
+      ),
+    );
+  }
+}
+class ButtonRowIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: Colors.red,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.add, color: Colors.white, size: 40,),
+              Text('Text', style: TextStyle(color: Colors.white, fontSize: 40),)
+            ],
+          ),
         ),
       ),
     );
@@ -37,12 +67,15 @@ class MyApp extends StatelessWidget {
 class ButtonCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 111,
-      color: Colors.blue,
-      child: Icon(Icons.add, color: Colors.white,),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 48.0),
+      child: Container(
+        width: double.infinity,
+        height: 111,
+        color: Colors.blue,
+        child: Icon(Icons.add, color: Colors.white,),
 
+      ),
     );
   }
 }
@@ -51,12 +84,19 @@ class ButtonCard extends StatelessWidget {
 class TextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 100,
-      alignment: Alignment.center,
-      color: Colors.greenAccent,
-      child: Text('Parmera sem mundial', style: TextStyle(color: Colors.lightGreen),)
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0), 
+          color: Colors.blueGrey,
+
+        ),
+        width: double.maxFinite, 
+        height: 100,
+        alignment: Alignment.center,
+        child: Text('Teste1', style: TextStyle(color: Colors.white, fontSize: 24))
+      ),
     );
   }
 }
