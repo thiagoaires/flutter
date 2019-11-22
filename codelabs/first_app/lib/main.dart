1b79e0b75e007d8874022f5eebece57fe0151882
thiagoaires <makarrao@gmail.com>
2019-11-22 11:59:17 -0300

    flutterando: icons

--- codelabs/first_app/lib/main.dart
+++ codelabs/first_app/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,10 +13,33 @@ class MyApp extends StatelessWidget {
           title: Text('first app'),
         ),
         body: Center(
-          child: Text('oi pai'),
+          child: RandomWords(),
+          //child: Text(wordPair.asPascalCase),
         ),
       ),
     );
   }
 }
+class RandomWords extends StatefulWidget{
 
+  @override
+  RandomWordsState createState() => RandomWordsState();
+
+}
+
+class RandomWordsState extends State<RandomWords>{
+
+  final _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions(){
+    
+  }
+
+  @override
+  Widget build(BuildContext context){
+    final WordPair wordPair = WordPair.random();
+
+    return Text(wordPair.asPascalCase);
+  }
+}
