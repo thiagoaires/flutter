13fd6b31609e3b7d42a49e90cad8b5e15a38d5f0
thiagoaires <makarrao@gmail.com>
2019-12-02 19:52:49 -0300

    modulo inicial

--- flutterando/design/hello_world/lib/main.dart
+++ flutterando/design/hello_world/lib/main.dart
@@ -14,13 +14,21 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
                   
-        body: Padding(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg"),
+              fit: BoxFit.cover
+            )
+          ),
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
 
             children: <Widget>[
+              Image.asset("assets/logo.png", width: 100,),
+              Image.network("https://images-americanas.b2w.io/spacey/2019/08/22/full_desk_festamobilecopiar2.png"),
               ButtonRowIcon(color: Colors.yellow, text: "Botao", icon: Icons.add_alarm),
               ButtonRowIcon(color: Colors.red, text: "top", icon: Icons.accessibility),
               Row(
