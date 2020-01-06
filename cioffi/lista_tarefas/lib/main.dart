520f1a4708a0cb1f5431fa16f55e64d5b1d085e0
thiagoaires <makarrao@gmail.com>
2020-01-06 19:21:46 -0300

    cioffi: lista todo's

--- cioffi/lista_tarefas/lib/main.dart
+++ cioffi/lista_tarefas/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -11,6 +14,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List _todoList = [];
+
+  final _todoController = TextEditingController();
+
+  Map<String, dynamic> _lastTodo = Map();
+  int _lastTodoPos;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _readData().then((data){
+      setState(() {
+        _todoList = json.decode(data);
+      });
+    });
+  }
+
+
+  void addTodo(){
+    setState(() {
+      Map<String, dynamic> newTodo = Map();
+      newTodo["title"] =  _todoController.text;
+      newTodo["ok"] = false;
+      _todoController.text = "";
+      _todoList.add(newTodo);
+
+      _saveFile();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,8 +53,118 @@ class _MyAppState extends State<MyApp> {
           centerTitle: true,
           title: Text("#TODO por TODOS"),
         ),
-        body: Text("TODO OPRESSOR"),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(16, 2, 8, 2),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        labelText: "Nova tarefa",
+                        labelStyle: TextStyle(
+                          color: Colors.blueAccent
+                        ),
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: addTodo,
+                    textColor: Colors.white,
+                    child: Text("Add"),
+                    color: Colors.blueAccent,
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _todoList.length,
+                itemBuilder: buildItem,
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget buildItem(context, index){
+    return Dismissible(
+      key: Key(index.toString()),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      onDismissed: (direction){
+        setState(() {
+          _lastTodoPos = index;
+          _lastTodo = _todoList[index];
+          _todoList.removeAt(index);
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastTodo["title"]} removida"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: (){
+                  setState(() {
+                    _todoList.insert(index, _lastTodo);
+                  });
+              }
+            ),
+            duration: Duration(seconds: 4),
+          );
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+        print(_lastTodo);
+        _saveFile();
+      },
+      child: CheckboxListTile(
+                  onChanged: (c){
+                    setState(() {
+                      _todoList[index]["ok"] = c;
+                    });
+                   _saveFile();
+                  },
+                  title: Text(_todoList[index]["title"]),
+                  value: _todoList[index]["ok"],
+                  secondary: CircleAvatar(
+                    child: Icon(
+                      _todoList[index]["ok"] ? 
+                      Icons.check :
+                      Icons.error
+                    ),
+                  ),
+                )
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveFile() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile(); 
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
