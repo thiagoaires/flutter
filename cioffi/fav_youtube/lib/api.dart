08d2d8f65531c9ea27ff99489dbc1a13bbdd4676
thiagoaires <makarrao@gmail.com>
2020-01-16 19:45:24 -0300

    cioffi: favoritos youtube[inicio c/ keys]

---
+++ cioffi/fav_youtube/lib/api.dart
@@ -0,0 +1,1 @@
+const API_KEY = "AIzaSyB4GLZPFSKoPk8Kux2i9-EytN9wmKKCXNY";
--- cioffi/fav_youtube/lib/main.dart
+++ cioffi/fav_youtube/lib/main.dart
@@ -1,5 +1,15 @@
+import 'package:fav_youtube/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    title: "Fluttertube",
+    home: Home(),
+  ));
 }
+
+/*
+"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+"https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_search&type=video&key=$API_KEY&maxResults=10&pageToken=$_nextToken"
+"http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
+*/
