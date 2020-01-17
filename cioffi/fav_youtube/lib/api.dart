5c50aa81b6c446d5845fcc44f1d2ed15abb215dd
thiago.aires <makarrao@gmail.com>
2020-01-17 00:53:48 -0300

    cioffi: favoritos youtube[wip]

--- cioffi/fav_youtube/lib/api.dart
+++ cioffi/fav_youtube/lib/api.dart
@@ -1,1 +1,12 @@
+import "package:http/http.dart" as http;
+
 const API_KEY = "AIzaSyB4GLZPFSKoPk8Kux2i9-EytN9wmKKCXNY";
+
+class Api{
+
+  search(String search) async{
+    http.Response response = await http.get(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&q=$search&type=video&key=$API_KEY&maxResults=10"
+      );
+  }
+}
