394291cf1f1af6d1c49c1fd757f368458cd1ace3
thiago.aires <makarrao@gmail.com>
2020-02-04 23:53:26 -0300

    balta: mobx

--- balta.io/youtube/pattern_projects/lib/views/signup.views.dart
+++ balta.io/youtube/pattern_projects/lib/views/signup.views.dart
@@ -3,11 +3,29 @@ import 'package:flutter/material.dart';
 class SignupView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hello signup"), 
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text("Cadastre-se"),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Nome",
+                    labelStyle: TextStyle(
+                      color: Theme.of(context).primaryColor
+                    )
+                  ),
+                ),
+              ],
+          ),),
+        )
       ),
-      body: Text("Hello my oldSignup"),
     );
   }
 }
