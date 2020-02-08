040d824e83702b800166a4eaf6d91f9ed848b84f
thiago.aires <makarrao@gmail.com>
2020-02-08 01:28:45 -0300

    alura: Persistencia dados

---
+++ alura/persistencia_interna/bytebank/bytebank/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Dashboard"),
+        ),
+      ),
+    );
+  }
+}
