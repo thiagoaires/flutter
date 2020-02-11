4bdff19407c401d75a18f3c82d8611fb540813b7
thiago.aires <makarrao@gmail.com>
2020-02-10 23:36:54 -0300

    Alura - Persistencia com flutter, cap 2 [ok]

--- alura/persistencia_interna/bytebank/bytebank/lib/main.dart
+++ alura/persistencia_interna/bytebank/bytebank/lib/main.dart
@@ -6,9 +6,56 @@ class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard"),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary
+        )
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset("assets/images/bytebank_logo.png"),
+              Container(
+                width: 150,
+                height: 100,
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(Icons.people, color: Colors.white,),
+                      Text("Contacts", style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500
+                      ),)
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
