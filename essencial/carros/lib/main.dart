cff734c9acb61eae1f14323abe6f26beb44771b7
thiagoaires <makarrao@gmail.com>
2020-03-17 11:31:06 -0300

    Essencial: Carros - Inicio

--- essencial/carros/lib/main.dart
+++ essencial/carros/lib/main.dart
@@ -8,10 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.white),
       home: LoginPage(),
     );
   }
--- essencial/carros/lib/pages/login_page.dart
+++ essencial/carros/lib/pages/login_page.dart
@@ -3,6 +3,33 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return ListView(
+      children: <Widget>[
+        Text("Login:"),
+        TextFormField(),
+        Text("Senha:"),
+        TextFormField(),
+        RaisedButton(
+          color: Colors.blue,
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          onPressed: () {},
+        )
+      ],
+    );
   }
 }
