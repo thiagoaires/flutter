09f26b2f2badfe963e00fe5425c01ee0f6cda497
thiagoaires <makarrao@gmail.com>
2021-07-21 22:11:03 -0300

    add Authentication UseCase

---
+++ manguinho/fordev/test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+
+void main(List<String> args) {
+  test('Should call http client with correct URL', () async {
+    final sut = RemoteAuthentication();
+  });
+}
+
+class RemoteAuthentication {}
